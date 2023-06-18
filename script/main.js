311e5367e4ba57eb4146618be70a1599008be438
Androginy <117454041+Androginy@users.noreply.github.com>
2023-06-18 15:48:06 +0200

    1

--- script/main.js
+++ script/main.js
@@ -3,10 +3,14 @@ const sliderArrowsItems = document.querySelectorAll(".slider-items-first a"),
       prevArrow = document.querySelector(".prev"),
       sliderFirstItems = document.querySelectorAll(".slider-items-second a"),
       nextFirst = document.querySelector(".next-first"),
-      prevFirst = document.querySelector(".prev-first");
+      prevFirst = document.querySelector(".prev-first"),
+      sliderThirdItems = document.querySelectorAll(".slider-items-third a"),
+      nextThird = document.querySelector(".next-third"),
+      prevThird = document.querySelector(".prev-third");
 
 let arrowsIndex = 0;
 let arrowsFirst = 0;
+let arrowsThird = 0;
 
 function changeSlideArrows(n) {
     arrowsIndex += 1;
@@ -22,6 +26,14 @@ function changeSlideFirst(n) {
     if(arrowsFirst < 0) arrowsFirst = sliderFirstItems.length - 1;
     sliderFirstItems.forEach(item => item.classList.add("hide"));
     sliderFirstItems[arrowsFirst].classList.remove("hide");
+}
+
+function changeSlideThird(n) {
+    arrowsThird += 1;
+    if(arrowsThird >= sliderThirdItems.length) arrowsThird = 0;
+    if(arrowsThird < 0) arrowsThird = sliderThirdItems.length - 1;
+    sliderThirdItems.forEach(item => item.classList.add("hide"));
+    sliderThirdItems[arrowsThird].classList.remove("hide");
 }
 
 nextArrow.addEventListener("click", function() {
@@ -40,6 +52,14 @@ prevFirst.addEventListener("click", function() {
     changeSlideFirst(-1);
 });
 
+nextThird.addEventListener("click", function() {
+    changeSlideThird(1);
+});
+
+prevThird.addEventListener("click", function() {
+    changeSlideThird(-1);
+});
+
 // hideSlide(sliderBtns, sliderItems);
 
 const navBtn = document.querySelectorAll(".menu a");
@@ -53,3 +73,5 @@ navBtn.forEach(btn => btn.addEventListener("click", function(event) {
         behavior : "smooth"
     });
 }));
+
+//новый слайдер
